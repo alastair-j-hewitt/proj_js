60b9bc0494eb2ae7a7a76c8bc4293c0c893baf99
Alastair Hewitt <alastairjohnhewitt@gmail.com>
2020-02-12 10:13:34 -0500

    paths switched to accomodate webhost

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+// app.listen(process.env.PORT || 3000, function(){
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
+
+var express = require('express');
+var app = express();
+
+app.get('/hello', function(req, res) {
+    console.log("World!!");
+})
+
+
+
+app.listen(process.env.PORT || 3000);
